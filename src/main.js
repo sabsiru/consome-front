a7ca3eb9ab1a6a3a7ae275229a71997dd3863c30
sabsiru <zero013@naver.com>
2025-10-29 19:57:23 +0900

    메인

--- src/main.js
+++ src/main.js
@@ -1,14 +1,33 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { useUserStore } from '@/stores/userStore'
+import api from '@/api/axios'
+
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
+
+const store = useUserStore()
+
+const token = localStorage.getItem('accessToken')
+if (token && !store.nickname) {
+  try {
+    const { data } = await api.get('/users/me')
+    store.setUserData(data)
+  } catch (e) {
+    console.error('자동 로그인 실패:', e)
+    if (e.response && e.response.status === 401) {
+      store.clearUser()
+    }
+  }
+}
 
 app.mount('#app')
