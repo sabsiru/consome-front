e36d619828425026e7160bfd9a6b0e0785a2f37a
sabsiru <zero013@naver.com>
2025-10-29 19:56:23 +0900

    token관련 추가

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -2,17 +2,34 @@ import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
-    nickname: '',
-    point: 0,
+    nickname: localStorage.getItem('nickname') || '',
+    point: Number(localStorage.getItem('point')) || 0,
+    token: localStorage.getItem('accessToken') || '',
+    role: localStorage.getItem('role') || ''
   }),
   actions: {
     setUserData(data) {
       this.nickname = data.nickname
       this.point = data.point
+      this.token = data.accessToken;
+      localStorage.setItem('nickname', data.nickname);
+      localStorage.setItem('point', data.point);
+      localStorage.setItem('accessToken', data.accessToken);
+      localStorage.setItem('role', data.role);
     },
     clearUser() {
       this.nickname = ''
       this.point = 0
+      this.token = ''
+      localStorage.removeItem('nickname');
+      localStorage.removeItem('point');
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('role')
+      localStorage.clear()
     },
   },
+  persist: {
+    storage: localStorage
+  }
 })
+
