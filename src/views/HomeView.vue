ca345a49b536862c1e07ba55fdc1927262f32e4d
sabsiru <zero013@naver.com>
2025-10-29 19:57:21 +0900

    메인

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,6 @@
   <main style="padding: 20px;">
     <h1>Consome 메인 페이지</h1>
 
-    <!-- 회원가입 버튼 -->
-    <button @click="goToRegister">회원가입</button>
-    <button @click="goToLogin">로그인</button>
   </main>
 </template>
 
@@ -13,10 +10,4 @@ import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const goToRegister = () => {
-  router.push('/register')
-}
-const goToLogin = () => {
-  router.push('/login')
-}
 </script>
