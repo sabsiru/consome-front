e775a839a02c357d8566cfc81bc9bc7742985601
sabsiru <zero013@naver.com>
2025-11-04 17:47:50 +0900

    admin

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -5,6 +5,7 @@
 
     <button @click="goToBoardManage" class="admin-btn">게시판 관리</button>
     <button @click="goToUserManage" class="admin-btn">회원 관리</button>
+    <button @click="goToUserManage" class="admin-btn">관리</button>
   </div>
 </template>
 
