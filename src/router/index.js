7e0e62b84a429397ce9076d99511673e3fb4693b
sabsiru <zero013@naver.com>
2026-01-10 16:56:09 +0900

    feat: 경로 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
+import BoardPostListView from '@/views/BoardPostListView.vue'
+import PostView from '../views/PostView.vue'
+import PostDetail from '../views/PostDetail.vue'
 import AdminRoutes from '@/router/adminRoutes.js'
 import { adminGuard, redirectIfLoggedIn } from '@/router/guards/authGuards.js'
 
@@ -11,6 +14,9 @@ const router = createRouter({
     { path: '/', name: 'home', component: HomeView },
     { path: '/register', name: 'register', component: RegisterView },
     { path: '/login', name: 'login', component: LoginView },
+    { path: '/boards/:boardId', name: 'BoardPosts', component: BoardPostListView, props: true },
+    { path: '/boards/:boardId/post', name: 'Post', component: PostView, props: true },
+    { path: '/posts/:postId', name: 'PostDetail', component: PostDetail, props: true },
     ...AdminRoutes,
   ],
 })
