f6fe22d841618aee2fa03d3d3fa0e114b6e4e7b8
sabsiru <zero013@naver.com>
2026-03-10 13:38:28 +0900

    feat: 비밀번호 재설정 라우트 추가
    - /password/reset-request → PasswordResetRequestView
    - /password/reset → PasswordResetView (query: token)

--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -1,5 +1,7 @@
 import UserProfileView from '@/views/user/UserProfileView.vue'
 import EmailVerifyView from '@/views/EmailVerifyView.vue'
+import PasswordResetRequestView from '@/views/PasswordResetRequestView.vue'
+import PasswordResetView from '@/views/PasswordResetView.vue'
 
 export default [
   {
@@ -12,5 +14,15 @@ export default [
     path: '/email/verify',
     name: 'EmailVerify',
     component: EmailVerifyView,
+  },
+  {
+    path: '/password/reset-request',
+    name: 'PasswordResetRequest',
+    component: PasswordResetRequestView,
+  },
+  {
+    path: '/password/reset',
+    name: 'PasswordReset',
+    component: PasswordResetView,
   },
 ]
