0ae9ec36508b188a4ff3405b1b93811ce317541b
sabsiru <zero013@naver.com>
2026-03-25 22:13:41 +0900

    feat: 인기 게시글 페이징 API 함수 추가
    - getPopularPostsPaged(page, size) 함수 추가
    - Spring Page 응답 형식 대응

--- src/api/navigationApi.js
+++ src/api/navigationApi.js
@@ -2,6 +2,11 @@ import api from '@/api/axios.js'
 
 export const getPopularPosts = () => api.get('/navigation/popular-posts')
 
+export const getPopularPostsPaged = (page = 0, size = 50) =>
+  api.get('/navigation/popular-posts/paged', {
+    params: { page, size }
+  })
+
 export const getPopularBoards = (boardLimit = 4, previewLimit = 5) =>
   api.get('/navigation/popular-boards', {
     params: { boardLimit, previewLimit }
